c748591e522a233164bd27cbd2348670e1be3a19
Joe Kachmar <me@jkachmar.com>
2020-05-17 02:38:18 -0400

    Bumps sources

--- default.nix
+++ default.nix
@@ -3,13 +3,22 @@
 let
   sources = import ./nix/sources.nix;
   haskell-nix = import sources.haskell-nix {};
+
+  # Helper function to construct an overlay that pins the haskell.nix Hackage
+  # and Stackage sources to the ones pinned by 'nix/sources.json'.
+  haskellNixOverlays = _self: super: {
+    haskell-nix = super.haskell-nix // {
+      hackageSrc = sources.hackage-nix;
+      stackageSrc = sources.stackage-nix;
+    };
+  };
   
   # All the haskell.nix overlays + any additional overlays we might like to
   # to include.
   #
   # To add additional overlays, append them as a list (be cautious and remember
   # that order of application matters with overlays).
-  overlays = haskell-nix.overlays;
+  overlays = haskell-nix.overlays ++ [ haskellNixOverlays ];
   
   # Arguments to apply to a given nixpkgs set with all the overlays applied.
   #
@@ -23,12 +32,11 @@ let
   # Import 
   inherit (pkgs.haskell-nix) cabalProject;
   inherit (pkgs.haskell-nix.haskellLib) cleanGit;
-  ghc = pkgs.buildPackages.haskell-nix.compiler.${ghcVersion};
 
 in
 
 cabalProject {
-  inherit ghc;
+  compiler-nix-name = ghcVersion;
   index-state = "2020-05-16T00:00:00Z";
 
   src = cleanGit { 
